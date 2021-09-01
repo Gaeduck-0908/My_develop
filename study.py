7187f6df90b61d6ee36b1026e13ddb18d041d276
Gaeduck <gms8757@naver.com>
2021-09-01 16:34:27 +0900

    debuging

--- study.py
+++ study.py
@@ -1,15 +1,26 @@
-d=[]
-for i in range(20) :
-  d.append([])
-  for j in range(20) : 
-    d[i].append(0)
+board = []
 
-n = int(input())
-for i in range(n) :
-  x, y = input().split()
-  d[int(x)][int(y)] = 1
+for i in range(10):
+    board.append(list(map(int , input().split())))
 
-for i in range(1, 20) :
-  for j in range(1, 20) : 
-    print(d[i][j], end=' ')
-  print()
+x,y = 1, 1
+
+while True:
+    if(board[x][y] == 0):
+        board[x][y] = 9
+    elif (board[x][y] == 2):
+        board[x][y] = 9
+        break
+
+    if ((board[x][y+1] == 1 and board [x+1][y] == 1)):
+        break
+
+    if (board[x][y+1] != 1):
+        y = y+1
+    elif (board[x+1][y] != 1):
+        x = x+1
+
+for j in range(10):
+    for k in range(10):
+        print(board[j][k], end=' ')
+    print() 
