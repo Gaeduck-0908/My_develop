39fcf88acadfe0dde72b335a731b77f93459e2c9
Gaeduck <gms8757@naver.com>
2021-11-12 15:42:57 +0900

    1231231233

--- Cstudy/Cstudy.c
+++ Cstudy/Cstudy.c
@@ -1,20 +1,19 @@
-﻿#include<stdio.h>
-
-int main(void)
-{
-    int c;
-    scanf("%d", &c);
-
-    for (int i = 0; i < c; i++)
-    {
-        int M;
-        int N = 0;
-        scanf("%d %d", &M, &N);
-        int Money[N];
+﻿#define 정수형 int
+#define 메인함수 main
+#define 소괄호열기 (
+#define 소괄호닫기 )
+#define 보이드 void
+#define 중괄호열기 {
+#define 중괄호닫기 }
+#define 출력 printf
+#define 문자열출력 "%s"
+#define 세미콜론 ;
 
 
 
-    }
+#include<stdio.h>
 
-    return 0;
-}
+정수형 메인함수 소괄호열기 보이드 소괄호닫기
+중괄호열기
+	출력 소괄호열기 문자열출력, "이게 한국어 코딩이다." 소괄호닫기 세미콜론
+중괄호닫기
--- reversing.py
+++ reversing.py
@@ -1,13 +1,10 @@
-a = ["?",";","F","J","D","n","v","8","d","w","8","l","R","u","l","y","R","m","m","t"]
-b = [20]
-
-for i in range (0,len(a)):
-    b[i] = ord(a[i]-1)
-
-for i in range (0,len(a)):
-    
-    b[i] ^= 0x11
-    b[i] ^= 0x1B
-    b[i] -= 3
-
-print(b)
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
+print('hello world')
