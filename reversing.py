2bd34c912127ab0c5980821c4e44a4fedf5b00a3
Gaeduck <gms8757@naver.com>
2021-11-19 16:13:37 +0900

    update

--- reversing.py
+++ reversing.py
@@ -1,10 +1,7 @@
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
+from itertools import product
+ 
+c = '0123456789abcdefghijklmnopqrstuvwxyz'
+ 
+for n in range(4,10):
+    for cc in product(c,repeat=n):
+        print 'G4HeulB'+"".join(cc)
